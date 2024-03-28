c3c5fad1dbd70eaa8e62ae66a866c5160d602230
jmlazaro <juan.lazaro2599@gmail.com>
2024-03-27 22:24:24 -0700

    change state structure to resemble image inputs; correct hint for dup chars; test_word arg

--- wordle.py
+++ wordle.py
@@ -15,23 +15,34 @@ ALPH_IDS: dict[str, int] = {char: i + 1 for i, char in enumerate(ALPH)}
 
 class Wordle():
     """
-    State values:
-        Rows correspond to attempts
-        Even indexed columns correspond to letter spaces.
-            E.g. self.state[0][0] = 1 means the first guess had an "a" in as the
-            first letter.
-        Odd indexed columns correspond to assesments/hints.
-            E.g. self.state[0][1] = 1 means that there is an "a" in the target
-            word, but it is in another space.
-        The codes for these columns is as follows:
-            -2: Character and hint space not used for short target word
+    State values: (attempt, character_space, char_hint_code)
+        Character codes are given by ALPH_IDS
+        Hint codes are follows:
+            -2: Character space not used for short target word
             -1: Character not in target word.
              0: Initial, "empty", state.
              1: Character used in target word, but in a different space.
              2: Character placed correctly.
+        E.g. self.state[0, 0, :]  = [1, 2] means the first guess had an "a" in
+             as the first letter and it was correct
+    State (summary) codes:
+        -1 = Lost
+         0 = Ongoing
+         1 = Won
     """
     max_attempts = 6
     max_chars = 15
+    char_channel = 0
+    hint_channel = 1
+    n_channels = 2
+    space_not_used = -2
+    not_used = -1
+    inital_empty = 0
+    elsewhere = 1
+    correct = 2
+    lost = -1
+    ongoing = 0
+    won = 1
     def __init__(self, words: list[str], target_len: int = None) -> None:
         """ Choose target and initialize state """
         if target_len:
@@ -39,65 +50,85 @@ class Wordle():
             words = tuple(w for w in words if len(w) == target_len)
             self.target = choice(words)
         else:
-            words = (w for w in words if len(w) > Wordle.max_chars)
+            words = tuple(w for w in words if len(w) <= Wordle.max_chars)
             self.target = choice(words)
             self.target_len = len(self.target)
 
         self.target_counts = Counter(self.target)
 
         self.attempts_made = 0
-        self.state = zeros((Wordle.max_attempts, Wordle.max_chars * 2))
-        self.state[:, self.target_len * 2:] = -2
+        self.state = zeros(
+            (Wordle.max_attempts, Wordle.max_chars, Wordle.n_channels)
+        )
+        self.state[:, self.target_len:, Wordle.hint_channel] = Wordle.not_used
 
     def guess(self, word: str) -> ndarray:
         """
-        Add (and return) guess assesment to state
-        Clips word if longer longer than self.target
+        Add guess assesment to state (and return)
         """
-        word = word.upper()[:self.target_len]
-        char_counts = defaultdict(lambda: 0)
-        for i, char in enumerate(word):
-            char_space = 2 * i
-            hint_space = char_space + 1
-            char_counts[char] += 1
-            self.state[self.attempts_made, char_space] = ALPH_IDS[char]
+        if len(word) != self.target_len:
+            raise ValueError
 
-            if char == self.target[i]:
-                self.state[self.attempts_made, hint_space] = 2
-            elif char in self.target:
-                if char_counts[char] <= self.target_counts[char]:
-                    self.state[self.attempts_made, hint_space] = 1
-                else:
-                    self.state[self.attempts_made, hint_space] = -1
+        word = word.upper()
+        char_tuples = list(enumerate(word))
+        char_counts = defaultdict(lambda: 0)
+
+        # Count correct chars first
+        # Prevents characters being marked as misplaced if the character is used
+        # twice in the guess, only once in the target and the later use is
+        # correct.
+        offset = 0
+        for char_space in range(self.target_len):
+            char = word[char_space]
+            space_tuple = (self.attempts_made, char_space)
+            self.state[*space_tuple, Wordle.char_channel] = ALPH_IDS[char]
+            if char == self.target[char_space]:
+                char_counts[char] += 1
+                self.state[*space_tuple, Wordle.hint_channel] = Wordle.correct
+                char_tuples.pop(char_space - offset)
+                offset += 1
+
+        for char_space, char in char_tuples:
+            space_tuple = (self.attempts_made, char_space, Wordle.hint_channel)
+            char_counts[char] += 1
+
+            if (
+                char in self.target
+                and char_counts[char] <= self.target_counts[char]
+            ):
+                self.state[*space_tuple] = Wordle.elsewhere
             else:
-                self.state[self.attempts_made, hint_space] = -1
+                self.state[*space_tuple] = Wordle.not_used
 
         self.attempts_made += 1
         return self.state
 
     def check_state(self) -> int:
-        """ Get state code: -1 = Lost, 0 = ongoing, 1 = Won """
         recent_attempt_success = all(
-            self.state[self.attempts_made - 1, hint] == 2
-            for hint in (2 * i + 1 for i in range(self.target_len))
+            self.state[self.attempts_made - 1, hint, 1] == 2
+            for hint in range(self.target_len)
         )
         if recent_attempt_success:
-            return 1
+            return Wordle.won
         elif self.attempts_made < Wordle.max_attempts:
-            return 0
+            return Wordle.ongoing
         else:
-            return -1
+            return Wordle.lost
 
 
 def main() -> None:
     """ Play Wordle on command line - FOR TESTING - NOT USER FRIENDLY """
     parser = ArgumentParser()
     parser.add_argument('-l', '--wlen', type=int, default=None)
+    parser.add_argument('-w', '--test_word')
     parser.add_argument('--vocab_file', default='corncob_caps.txt')
     args = parser.parse_args()
 
-    with open(args.vocab_file) as f:
-        all_words = tuple(f.read().split('\n'))
+    if args.test_word:
+        all_words = [args.test_word.upper()]
+    else:
+        with open(args.vocab_file) as f:
+            all_words = tuple(f.read().split('\n'))
 
     game = Wordle(all_words, target_len=args.wlen)
     while game.check_state() == 0:
@@ -107,7 +138,7 @@ def main() -> None:
         )
         game.guess(guess)
         print('Current state:')
-        print(game.state[:game.attempts_made, :2 * game.target_len])
+        print(game.state[:game.attempts_made, :game.target_len])
 
     if game.check_state() == 1:
         print('You Won!')
